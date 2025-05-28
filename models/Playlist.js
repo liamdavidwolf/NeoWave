a727f11a24f37f08402fd6a30168fc5c4d436ec9
liamdavidwolf <160881788+liamdavidwolf@users.noreply.github.com>
2025-05-28 16:26:15 -0500

    Update Playlist.js
    Se agregan cambios para que el UserId no sea necesario al momento de crear playlists

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const playlistSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // <-- aquí
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },  // <-- aquí
   name: { type: String, required: true },
   songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }],
   cover: String, // URL o nombre del archivo de imagen
